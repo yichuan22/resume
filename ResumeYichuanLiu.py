3295aecb809b15d4c0ba058b0b3acec6f5c86927
yichuan22 <144292134+yichuan22@users.noreply.github.com>
2023-11-28 20:56:46 -0500

    Update ResumeYichuanLiu.py

--- ResumeYichuanLiu.py
+++ ResumeYichuanLiu.py
@@ -1,21 +1,13 @@
 import streamlit as st
-import base64
 
 # Assuming your resume is in the same directory as your Streamlit app.
 RESUME_PATH = 'Liu_Yichuan_Resume.pdf'
 
-def get_base64_encoded_pdf(pdf_file_path):
-    with open(pdf_file_path, "rb") as pdf_file:
-        return base64.b64encode(pdf_file.read()).decode('utf-8')
-
 # Main app
 st.title("Resume Viewer")
 
-# Display PDF in the app
-with open(RESUME_PATH, "rb") as pdf_file:
-    base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
-pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf"></iframe>'
-st.markdown(pdf_display, unsafe_allow_html=True)
+# Display link to open PDF in new tab
+st.markdown(f'[Open Resume](Liu_Yichuan_Resume.pdf)', unsafe_allow_html=True)
 
 # Download link
 with open(RESUME_PATH, 'rb') as pdf_file:
@@ -25,3 +17,4 @@ with open(RESUME_PATH, 'rb') as pdf_file:
         file_name="Yichuan_Liu_Resume.pdf",
         mime='application/pdf'
     )
+
