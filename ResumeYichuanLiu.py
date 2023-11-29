47224041d6b9595ff8edd756988a08f962fa0a2a
yichuan22 <144292134+yichuan22@users.noreply.github.com>
2023-11-28 20:50:55 -0500

    Update ResumeYichuanLiu.py

--- ResumeYichuanLiu.py
+++ ResumeYichuanLiu.py
@@ -1,28 +1,27 @@
 import streamlit as st
 import base64
 
-# Function to encode the PDF for embedding
+# Assuming your resume is in the same directory as your Streamlit app.
+RESUME_PATH = 'Liu_Yichuan_Resume.pdf'
+
 def get_base64_encoded_pdf(pdf_file_path):
     with open(pdf_file_path, "rb") as pdf_file:
         return base64.b64encode(pdf_file.read()).decode('utf-8')
 
 # Main app
-def main():
-    st.title("Yichuan Liu's Resume")
+st.title("Resume Viewer")
 
-    # Resume display
-    base64_pdf = get_base64_encoded_pdf('Liu_Yichuan_Resume.pdf')
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+# Display PDF in the app
+with open(RESUME_PATH, "rb") as pdf_file:
+    base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
+pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf"></iframe>'
+st.markdown(pdf_display, unsafe_allow_html=True)
 
-    # Download link
-    with open('Liu_Yichuan_Resume.pdf', 'rb') as pdf_file:
-        st.download_button(
-            label="Download Resume",
-            data=pdf_file,
-            file_name="Yichuan_Liu_Resume.pdf",
-            mime='application/pdf'
-        )
-
-if __name__ == "__main__":
-    main()
+# Download link
+with open(RESUME_PATH, 'rb') as pdf_file:
+    st.download_button(
+        label="Download Resume",
+        data=pdf_file,
+        file_name="Yichuan_Liu_Resume.pdf",
+        mime='application/pdf'
+    )
